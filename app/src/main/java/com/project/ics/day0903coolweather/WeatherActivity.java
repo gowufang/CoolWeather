7ae6379616a6d4769ca2f949b7fd24b43f452555
gowufang <gowufang@gmail.com>
2017-09-26 21:47:18 +0800

    add the layouts with the relevant to weather

---
+++ app/src/main/java/com/project/ics/day0903coolweather/WeatherActivity.java
@@ -0,0 +1,11 @@
+package com.project.ics.day0903coolweather;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 2017/9/26.
+ */
+
+public class WeatherActivity extends Activity {
+
+}
